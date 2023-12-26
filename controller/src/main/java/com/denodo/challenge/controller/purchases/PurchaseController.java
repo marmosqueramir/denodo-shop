cdeffcbbb0f24d039adfc91eb57964f31211218a
marmosqueramir <marmosqueramir@gmail.com>
2023-12-26 01:35:49 +0100

    Add openapi

--- controller/src/main/java/com/denodo/challenge/controller/purchases/PurchaseController.java
+++ controller/src/main/java/com/denodo/challenge/controller/purchases/PurchaseController.java
@@ -2,6 +2,12 @@ package com.denodo.challenge.controller.purchases;
 
 import com.denodo.challenge.dto.PurchasesForMostRepeatedAgeByDateDTO;
 import com.denodo.challenge.service.purchases.interfaces.PurchaseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -18,6 +24,7 @@ import java.util.List;
 @RequestMapping("/purchases")
 public class PurchaseController {
 
+    private static final String DATE_FORMAT = "yyyy-MM-dd HH:mm";
     private PurchaseService purchaseService;
 
     @Autowired
@@ -25,10 +32,15 @@ public class PurchaseController {
         this.purchaseService = purchaseService;
     }
 
+    @Operation(summary = "Get the user and the value of purchases made by the most frequent age group within a given time range")
+    @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "Found the data of user and purchase",
+                  content = { @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = PurchasesForMostRepeatedAgeByDateDTO.class))) })
+    })
     @GetMapping("/totalPurchasesForMostRepeatedAgeByDate")
     public ResponseEntity<List<PurchasesForMostRepeatedAgeByDateDTO>> totalPurchasesForMostRepeatedAgeByDate(
-            @RequestParam @NotNull @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm") Date initDateTime,
-            @RequestParam @NotNull @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm") Date endDateTime) {
+            @RequestParam @NotNull @DateTimeFormat(pattern = DATE_FORMAT) Date initDateTime,
+            @RequestParam @NotNull @DateTimeFormat(pattern = DATE_FORMAT) Date endDateTime) {
         try {
             List<PurchasesForMostRepeatedAgeByDateDTO> result =
                     purchaseService.totalPurchasesForMostRepeatedAgeByDate(initDateTime, endDateTime);
